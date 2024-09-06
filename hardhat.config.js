d6ba252ebd47e6c4b12e35ba24ff9247c8a9603f
Sneha-p1 <snehaparambadan@gmail.com>
2024-09-06 11:13:06 +0530

    maincert

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
-
+require("@nomicfoundation/hardhat-toolbox")
+require('dotenv').config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork: "localhost",
+  defaultNetwork: "infurahol",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545/"
+      url:"http://127.0.0.1:8545/"
     },
-    hardhat: {
-      // See its defaults
-    }
+    infurahol: {
+      url: "https://sepolia.infura.io/v3/d9c402d758404d1194e5b55729408396",
+      accounts:[process.env.Privatekey]
+    },
   },
   solidity: "0.8.20",
 };
